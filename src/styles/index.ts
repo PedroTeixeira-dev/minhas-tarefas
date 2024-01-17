c7919261c22a5841053b1fec2380ae95a3dce99a
Pedro Teixeira <pedro15at@gmail.com>
2024-01-17 09:55:35 -0300

    Criando e estilizando o aside

---
+++ src/styles/index.ts
@@ -0,0 +1,16 @@
+import styled, { createGlobalStyle } from 'styled-components'
+
+const EstiloGlobal = createGlobalStyle`
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif, sans-serif;
+}`
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 224px auto;
+`
+
+export default EstiloGlobal
