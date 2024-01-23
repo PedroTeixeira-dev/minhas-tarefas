0f861d74f0e7c5f0bd26a819bc9d88ff83f352a8
Pedro Teixeira <pedro15at@gmail.com>
2024-01-23 19:46:19 -0300

    estilizando as tarefas

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,7 +1,43 @@
+import Tarefa from '../../components/Tarefa'
+import { Container } from './styles'
+
+const tarefas = [
+  {
+    titulo: 'Estudar Typescript',
+    descricao: 'Ver a aula 3',
+    prioridade: 'Importante',
+    status: 'pendente'
+  },
+  {
+    titulo: 'pagar a conta de internet',
+    descricao: 'Pegar o boleto e pagar',
+    prioridade: 'urgente',
+    status: 'Concluida'
+  },
+  {
+    titulo: 'Ir para academia',
+    descricao: 'Treino de perna',
+    prioridade: 'Importante',
+    status: 'pendente'
+  }
+]
+
 const ListaDeTarefas = () => (
-  <main>
+  <Container>
     <p>2 Tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
-  </main>
+    <ul>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.descricao}
+            titulo={t.titulo}
+            status={t.status}
+            prioridade={t.prioridade}
+          />
+        </li>
+      ))}
+    </ul>
+  </Container>
 )
 
 export default ListaDeTarefas
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -6,6 +6,7 @@ const EstiloGlobal = createGlobalStyle`
   padding: 0;
   box-sizing: border-box;
   font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif, sans-serif;
+  list-style: none;
 }`
 
 export const Container = styled.div`
