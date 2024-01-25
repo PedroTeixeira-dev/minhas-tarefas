88de0eb3cbf4dbc88be9edb5f58ca6d5d828b163
Pedro Teixeira <pedro15at@gmail.com>
2024-01-25 12:18:37 -0300

    Fazendo a logica dos botoes de salvar, cancelar e editar

--- src/store/reducers/tarefas.ts
+++ src/store/reducers/tarefas.ts
@@ -2,38 +2,54 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import Tarefa from '../../models/Tarefa'
 import * as enums from '../../utils/enums/tarefa'
 
+type TarefasState = {
+  itens: Tarefa[]
+}
+
+const initialState: TarefasState = {
+  itens: [
+    {
+      id: 1,
+      titulo: 'Estudar JS',
+      descricao: 'Estudar JS revendo aula do modulo 4',
+      prioridade: enums.Prioridade.URGENTE,
+      status: enums.Status.PENDENTE
+    },
+    {
+      id: 2,
+      titulo: 'Estudar Material de apoio',
+      descricao: 'Estudar JS material de apoio do modulo 3',
+      prioridade: enums.Prioridade.NORMAL,
+      status: enums.Status.PENDENTE
+    },
+    {
+      id: 3,
+      titulo: 'Estudar TS',
+      descricao: 'Estudar TS revendo aula do modulo 4',
+      prioridade: enums.Prioridade.IMPORTANTE,
+      status: enums.Status.CONCLUIDO
+    }
+  ]
+}
+
 const tarefasSlice = createSlice({
   name: 'tarefas',
-  initialState: [
-    new Tarefa(
-      'Estudar JavaScript',
-      enums.Prioridade.IMPORTANTE,
-      enums.Status.PENDENTE,
-      '',
-      1
-    ),
-    new Tarefa(
-      'Estudar TypeScript',
-      enums.Prioridade.URGENTE,
-      enums.Status.CONCLUIDO,
-      'Rever aula 2 do modulo',
-      2
-    ),
-    new Tarefa(
-      'Estudar React',
-      enums.Prioridade.URGENTE,
-      enums.Status.PENDENTE,
-      'Praticar o useEffect',
-      3
-    )
-  ],
+  initialState,
   reducers: {
     remover: (state, action: PayloadAction<number>) => {
-      state = state.filter((tarefa) => tarefa.id !== action.payload)
+      state.itens = state.itens.filter((tarefa) => tarefa.id !== action.payload)
+    },
+    editar: (state, action: PayloadAction<Tarefa>) => {
+      const indexDaTarefa = state.itens.findIndex(
+        (t) => t.id === action.payload.id
+      )
+      if (indexDaTarefa >= 0) {
+        state.itens[indexDaTarefa] = action.payload
+      }
     }
   }
 })
 
-export const { remover } = tarefasSlice.actions
+export const { remover, editar } = tarefasSlice.actions
 
 export default tarefasSlice.reducer
