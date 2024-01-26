9b2e2b26bcbef9979437cc32311c9f0d682fe9ba
Pedro Teixeira <pedro15at@gmail.com>
2024-01-26 10:36:00 -0300

    fazendo o filtro de busca

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,21 +1,34 @@
+import { useDispatch, useSelector } from 'react-redux'
 import FiltroCard from '../../components/FiltroCard'
 
 import * as S from './styles'
+import { RootReducer } from '../../store'
+import { aletraTermo } from '../../store/reducers/filtro'
 
-const BarraLateral = () => (
-  <S.Aside>
-    <div>
-      <S.Campo type="text" placeholder="Buscar" />
-      <S.Filtros>
-        <FiltroCard legenda="pendentes" contador={1} />
-        <FiltroCard legenda="Concluidas" contador={2} />
-        <FiltroCard legenda="Urgentes" contador={3} />
-        <FiltroCard legenda="Importantes" contador={2} />
-        <FiltroCard legenda="Normal" contador={1} />
-        <FiltroCard legenda="Todas" contador={9} ativo />
-      </S.Filtros>
-    </div>
-  </S.Aside>
-)
+const BarraLateral = () => {
+  const dispatch = useDispatch()
+  const { termo } = useSelector((state: RootReducer) => state.filtro)
+
+  return (
+    <S.Aside>
+      <div>
+        <S.Campo
+          type="text"
+          placeholder="Buscar"
+          value={termo}
+          onChange={(evento) => dispatch(aletraTermo(evento.target.value))}
+        />
+        <S.Filtros>
+          <FiltroCard legenda="pendentes" contador={1} />
+          <FiltroCard legenda="Concluidas" contador={2} />
+          <FiltroCard legenda="Urgentes" contador={3} />
+          <FiltroCard legenda="Importantes" contador={2} />
+          <FiltroCard legenda="Normal" contador={1} />
+          <FiltroCard legenda="Todas" contador={9} ativo />
+        </S.Filtros>
+      </div>
+    </S.Aside>
+  )
+}
 
 export default BarraLateral
